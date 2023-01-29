e1a3a4ba4c5b829315daf99392ce77435fa19da3
Jeroen Schmit <jeroen@slimndap.com>
2023-01-29 17:01:20 +0100

    Added new tickets template to the front end.

--- includes/Frontend/Frontend.php
+++ includes/Frontend/Frontend.php
@@ -2,6 +2,7 @@
 namespace TBTEC\Frontend;
 
 add_action( 'init', __NAMESPACE__.'\add_tickets_button_to_single_event' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__.'\enqueue_scripts' );
 
 function add_tickets_button_to_single_event() {
 	
@@ -35,11 +36,42 @@ function do_single_event_tickets_button() {
 		return;
 	}
 	
-	?><div class="tribe-common event-tickets">
-		<a class="tribe-common-c-btn-border" href="<?php
-			echo esc_attr( $tickets_button_url );
-		?>" style="color: var( --tec-color-background ); background-color: var(--tec-color-accent-primary); width: auto;"><?php
-			echo esc_html( $settings[ 'ticket_buttons_label' ] ); 
-		?></a>
+	?><div class="tbtec-tickets tribe-common">
+		
+		<h2 class="tribe-common-h4 tribe-common-h--alt">Tickets</h2>
+		
+		<div class="tbtec-ticket">
+			<div class="tbtec-ticket-title tribe-common-h7 tribe-common-h6--min-medium">
+				Normaal	
+			</div>
+			<div class="tbtec-ticket-price tribe-common-b2 tribe-common-b3--min-medium">
+				€ 20,00
+			</div>
+		</div>
+
+		<div class="tbtec-ticket">
+			<div class="tbtec-ticket-title tribe-common-h7 tribe-common-h6--min-medium">
+				Normaal	
+			</div>
+			<div class="tbtec-ticket-price tribe-common-b2 tribe-common-b3--min-medium">
+				€ 20,00
+			</div>
+		</div>
+
+		<div class="tbtec_ticket-button">
+			<a class="tribe-common-c-btn-border" href="<?php
+				echo esc_attr( $tickets_button_url );
+			?>" sstyle="color: var( --tec-color-background ); background-color: var(--tec-color-accent-primary); width: auto;"><?php
+				echo esc_html( $settings[ 'ticket_buttons_label' ] ); 
+			?></a>
+		</div>
+
 	</div><?php
+	
+}
+
+function enqueue_scripts() {
+	
+	wp_enqueue_style( 'tectb', \TBTEC\PLUGIN_URI.'/assets/css/style.css', array(), \TBTEC\VERSION );
+	
 }
